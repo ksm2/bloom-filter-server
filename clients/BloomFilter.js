dc12ade3da686a5487e63c1cb470829206f18c36
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-06-18 01:34:46 +0200

    Refactor Node.js client

---
+++ clients/BloomFilter.js
@@ -0,0 +1,96 @@
+const net = require('net')
+
+class BloomFilter {
+
+  /**
+   * @param {string} host
+   * @param {number} port
+   */
+  constructor(host = '127.0.0.1', port = 1337) {
+    this.host = host
+    this.port = port
+    this.isOpen = false
+    this.client = new net.Socket()
+    this.nextData = null
+    this.nextError = null
+
+    this.client.on('close', () => {
+      this.isOpen = false
+    })
+
+    this.client.on('data', (data) => {
+      if (this.nextData) {
+        this.nextData(data)
+      }
+    })
+
+    this.client.on('error', (error) => {
+      if (this.nextError) {
+        this.nextError(error)
+      }
+    })
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.client.connect(this.port, this.host, () => {
+        this.isOpen = true
+        resolve()
+      })
+    })
+  }
+
+  end() {
+    this.client.end()
+  }
+
+  send(data) {
+    return new Promise((resolve, reject) => {
+      this.nextData = resolve
+      this.nextError = reject
+      this.client.write(data)
+    })
+  }
+
+  async add(...items) {
+    const response = await this.send(`ADD ${items.join(' ')}\n`)
+    if (response.toString() !== 'OK.\n') {
+      throw new Error(`Server responded with ${response}`)
+    }
+  }
+
+  async remove(...items) {
+    const response = await this.send(`RMV ${items.join(' ')}\n`)
+    if (response.toString() !== 'OK.\n') {
+      throw new Error(`Server responded with ${response}`)
+    }
+  }
+
+  async has(item) {
+    const r = await this.send(`HAS ${item}\n`)
+    switch (r.toString()) {
+      case 'Yes.\n':
+        return true
+      case 'No.\n':
+        return false
+      default:
+        throw new Error(`Server responded with ${r}`)
+    }
+  }
+
+  async count(item) {
+    const response = await this.send(`CNT ${item}\n`)
+    const s = response.toString()
+    if (s.startsWith('ERROR.')) {
+      throw new Error(s)
+    }
+
+    return parseInt(s, 10)
+  }
+
+  async binary() {
+    return await this.send('BIN')
+  }
+}
+
+exports.BloomFilter = BloomFilter
--- clients/test.js
+++ clients/test.js
@@ -1,51 +1,31 @@
-const net = require('net')
+const { BloomFilter } = require('./BloomFilter')
 
-const client = new net.Socket()
+async function main() {
+  const bf = new BloomFilter()
+  await bf.connect()
 
-function send(data) {
-  return new Promise((resolve, reject) => {
-    client.once('error', err => reject(err))
-    client.once('data', data => resolve(data))
-    client.write(data)
-  })
+  console.log(await bf.has('a'))
+  console.log(await bf.count('a'))
+
+  await bf.add('a')
+
+  console.log(await bf.has('a'))
+  console.log(await bf.count('a'))
+
+  await bf.add('a')
+  console.log(await bf.count('a'))
+
+  await bf.add('a')
+  console.log(await bf.count('a'))
+
+  await bf.remove('a')
+  await bf.remove('a')
+  await bf.remove('a')
+
+  console.log(await bf.has('a'))
+  console.log(await bf.count('a'))
+
+  bf.end()
 }
 
-async function add(item) {
-  const response = await send(`ADD ${item}`)
-  if (response.toString() !== 'OK.\n') {
-    throw new Error(`Server responded with ${response}`)
-  }
-}
-
-async function has(item) {
-  const r = await send(`HAS ${item}`)
-  switch (r.toString()) {
-    case 'Yes.\n':
-      return true
-    case 'No.\n':
-      return false
-    default:
-      throw new Error(`Server responded with ${r}`)
-  }
-}
-
-async function bits() {
-  return await send('BITS')
-}
-
-client.connect(1337, '127.0.0.1', async () => {
-  console.log('Connected to server!')
-
-  console.log(await has('a'))
-  await add('a')
-  console.log(await has('a'))
-
-  const buffer = await bits()
-  console.log(buffer)
-
-  client.destroy()
-})
-
-client.on('close', function () {
-  console.log('Connection closed')
-})
+main().catch(console.error)

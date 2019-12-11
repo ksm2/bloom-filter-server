756260bb18b40eb9f104b85a3966d4ca13f4a825
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2019-12-12 00:28:07 +0100

    Improve JavaScript code

--- clients/BloomFilter.js
+++ clients/BloomFilter.js
@@ -44,30 +44,30 @@ class BloomFilter {
     this.client.end()
   }
 
-  send(data) {
+  send(cmd, ...data) {
     return new Promise((resolve, reject) => {
       this.nextData = resolve
       this.nextError = reject
-      this.client.write(data)
+      this.client.write(`${cmd} ${data.join(' ')}\n`)
     })
   }
 
   async add(...items) {
-    const response = await this.send(`ADD ${items.join(' ')}\n`)
+    const response = await this.send('ADD', ...items)
     if (response.toString() !== 'OK.\n') {
       throw new Error(`Server responded with ${response}`)
     }
   }
 
   async remove(...items) {
-    const response = await this.send(`RMV ${items.join(' ')}\n`)
+    const response = await this.send('RMV', ...items)
     if (response.toString() !== 'OK.\n') {
       throw new Error(`Server responded with ${response}`)
     }
   }
 
   async has(item) {
-    const r = await this.send(`HAS ${item}\n`)
+    const r = await this.send('HAS', item)
     switch (r.toString()) {
       case 'Yes.\n':
         return true
@@ -79,7 +79,7 @@ class BloomFilter {
   }
 
   async count(item) {
-    const response = await this.send(`CNT ${item}\n`)
+    const response = await this.send('CNT', item)
     const s = response.toString()
     if (s.startsWith('ERROR.')) {
       throw new Error(s)
